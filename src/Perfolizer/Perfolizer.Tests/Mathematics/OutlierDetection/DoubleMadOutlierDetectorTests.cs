b344ca3018aa91b2c399ebb749e32bcf4a3b4329
Andrey Akinshin <andrey.akinshin@jetbrains.com>
2020-06-21 09:45:59 +0300

    More unit tests for outlier detectors

--- src/Perfolizer/Perfolizer.Tests/Mathematics/OutlierDetection/DoubleMadOutlierDetectorTests.cs
+++ src/Perfolizer/Perfolizer.Tests/Mathematics/OutlierDetection/DoubleMadOutlierDetectorTests.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using JetBrains.Annotations;
-using Perfolizer.Collections;
 using Perfolizer.Mathematics.OutlierDetection;
 using Perfolizer.Mathematics.QuantileEstimators;
 using Perfolizer.Tests.Common;
@@ -11,36 +10,71 @@ namespace Perfolizer.Tests.Mathematics.OutlierDetection
 {
     public class DoubleMadOutlierDetectorTests : OutlierDetectorTests
     {
-        private static readonly IQuantileEstimator QuantileEstimator = SimpleQuantileEstimator.Instance;
-
         public DoubleMadOutlierDetectorTests([NotNull] ITestOutputHelper output) : base(output)
         {
         }
 
-        protected override IOutlierDetector CreateOutlierDetector(IReadOnlyList<double> values) => DoubleMadOutlierDetector.Create(values,
-            quantileEstimator: QuantileEstimator);
-
-        private static readonly IDictionary<string, TestData> TestDataMap = new Dictionary<string, TestData>
+        /// <summary>
+        /// Data cases for SimpleQuantileEstimator
+        /// </summary>
+        private static readonly IDictionary<string, TestData> SimpleQeTestDataMap = new Dictionary<string, TestData>
         {
-            {"Case1", new TestData(new double[] {1, 4, 4, 4, 5, 5, 5, 5, 7, 7, 8, 10, 16, 30}, new double[] {1, 16, 30})}
+            {"Case1", new TestData(new double[] {1, 4, 4, 4, 5, 5, 5, 5, 7, 7, 8, 10, 16, 30}, new double[] {1, 16, 30})},
+            {"Real0", new TestData(RealDataSet.X0, new double[] {38594, 39075})},
+            {"Real1", new TestData(RealDataSet.X1, new double[] {0, 0, 0, 0, 1821})},
+            {"Real2", new TestData(RealDataSet.X2, new double[] {95, 4364})},
+            {"Real3", new TestData(RealDataSet.X3, new double[] {1067, 1085, 1133, 1643, 4642})},
+            {"Real4", new TestData(RealDataSet.X4, new double[] {})},
+            {"Beta0", new TestData(BetaDataSet.X0, new double[] {})},
+            {"Beta1", new TestData(BetaDataSet.X1, new double[] {3071})},
+            {"Beta2", new TestData(BetaDataSet.X2, new double[] {3642})},
+            {"MBeta_Lower1", new TestData(ModifiedBetaDataSet.Lower1, new double[] {-2000, 3612})},
+            {"MBeta_Lower2", new TestData(ModifiedBetaDataSet.Lower2, new double[] {-2001, -2000, 3612})},
+            {"MBeta_Lower3", new TestData(ModifiedBetaDataSet.Lower3, new double[] {-2002, -2001, -2000, 3612})},
+            {"MBeta_Upper1", new TestData(ModifiedBetaDataSet.Upper1, new double[] {3612, 6000})},
+            {"MBeta_Upper2", new TestData(ModifiedBetaDataSet.Upper2, new double[] {6000, 6001})},
+            {"MBeta_Upper3", new TestData(ModifiedBetaDataSet.Upper3, new double[] {6000, 6001, 6002})},
+            {"MBeta_Both0", new TestData(ModifiedBetaDataSet.Both0, new double[] {-2000, 6000})},
+            {"MBeta_Both1", new TestData(ModifiedBetaDataSet.Both1, new double[] {-2001, -2000, 6000, 6001})},
+            {"MBeta_Both2", new TestData(ModifiedBetaDataSet.Both2, new double[] {-2002, -2001, -2000, 6000, 6001, 6002})}
         };
 
-        [UsedImplicitly] public static TheoryData<string> TestDataKeys = TheoryDataHelper.Create(TestDataMap.Keys);
+        [UsedImplicitly] public static TheoryData<string> SimpleQeTestDataKeys = TheoryDataHelper.Create(SimpleQeTestDataMap.Keys);
 
         [Theory]
-        [MemberData(nameof(TestDataKeys))]
-        public void DoubleMadOutlierDetectorTest([NotNull] string testDataKey) => Check(TestDataMap[testDataKey]);
+        [MemberData(nameof(SimpleQeTestDataKeys))]
+        public void DoubleMadOutlierDetectorSimpleQeTest([NotNull] string testDataKey) => Check(SimpleQeTestDataMap[testDataKey],
+            values => DoubleMadOutlierDetector.Create(values, quantileEstimator: SimpleQuantileEstimator.Instance));
 
-        protected override void DumpDetails(TestData testData)
+        /// <summary>
+        /// Data cases for HarrellDavisQuantileEstimator
+        /// </summary>
+        private static readonly IDictionary<string, TestData> HdQeTestDataMap = new Dictionary<string, TestData>
         {
-            var values = testData.Values.ToSorted();
-            double median = QuantileEstimator.GetMedian(values);
-            double lowerMad = MedianAbsoluteDeviation.CalcLowerMad(values, quantileEstimator: QuantileEstimator);
-            double upperMad = MedianAbsoluteDeviation.CalcUpperMad(values, quantileEstimator: QuantileEstimator);
+            {"Real0", new TestData(RealDataSet.X0, new double[] {38594, 39075})},
+            {"Real1", new TestData(RealDataSet.X1, new double[] {0, 0, 0, 0, 1821})},
+            {"Real2", new TestData(RealDataSet.X2, new double[] {95, 4364})},
+            {"Real3", new TestData(RealDataSet.X3, new double[] {1067, 1085, 1133, 1643, 4642})},
+            {"Real4", new TestData(RealDataSet.X4, new double[] {})},
+            {"Beta0", new TestData(BetaDataSet.X0, new double[] {})},
+            {"Beta1", new TestData(BetaDataSet.X1, new double[] {3071})},
+            {"Beta2", new TestData(BetaDataSet.X2, new double[] {3642})},
+            {"MBeta_Lower1", new TestData(ModifiedBetaDataSet.Lower1, new double[] {-2000})},
+            {"MBeta_Lower2", new TestData(ModifiedBetaDataSet.Lower2, new double[] {-2001, -2000})},
+            {"MBeta_Lower3", new TestData(ModifiedBetaDataSet.Lower3, new double[] {-2002, -2001, -2000})},
+            {"MBeta_Upper1", new TestData(ModifiedBetaDataSet.Upper1, new double[] {6000})},
+            {"MBeta_Upper2", new TestData(ModifiedBetaDataSet.Upper2, new double[] {6000, 6001})},
+            {"MBeta_Upper3", new TestData(ModifiedBetaDataSet.Upper3, new double[] {6000, 6001, 6002})},
+            {"MBeta_Both0", new TestData(ModifiedBetaDataSet.Both0, new double[] {-2000, 6000})},
+            {"MBeta_Both1", new TestData(ModifiedBetaDataSet.Both1, new double[] {-2001, -2000, 6000, 6001})},
+            {"MBeta_Both2", new TestData(ModifiedBetaDataSet.Both2, new double[] {-2002, -2001, -2000, 6000, 6001, 6002})}
+        };
 
-            Output.WriteLine($"Median   = {median.ToString(TestCultureInfo.Instance)}");
-            Output.WriteLine($"LowerMAD = {lowerMad.ToString(TestCultureInfo.Instance)}");
-            Output.WriteLine($"UpperMAD = {upperMad.ToString(TestCultureInfo.Instance)}");
-        }
+        [UsedImplicitly] public static TheoryData<string> HdQeTestDataKeys = TheoryDataHelper.Create(HdQeTestDataMap.Keys);
+
+        [Theory]
+        [MemberData(nameof(HdQeTestDataKeys))]
+        public void DoubleMadOutlierDetectorHdQeTest([NotNull] string testDataKey) => Check(HdQeTestDataMap[testDataKey],
+            values => DoubleMadOutlierDetector.Create(values, quantileEstimator: HarrellDavisQuantileEstimator.Instance));
     }
 }
--- src/Perfolizer/Perfolizer.Tests/Mathematics/OutlierDetection/MadOutlierDetectorTests.cs
+++ src/Perfolizer/Perfolizer.Tests/Mathematics/OutlierDetection/MadOutlierDetectorTests.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using JetBrains.Annotations;
-using Perfolizer.Collections;
 using Perfolizer.Mathematics.OutlierDetection;
 using Perfolizer.Mathematics.QuantileEstimators;
 using Perfolizer.Tests.Common;
@@ -11,38 +10,75 @@ namespace Perfolizer.Tests.Mathematics.OutlierDetection
 {
     public class MadOutlierDetectorTests : OutlierDetectorTests
     {
-        private static readonly IQuantileEstimator QuantileEstimator = SimpleQuantileEstimator.Instance;
-
         public MadOutlierDetectorTests([NotNull] ITestOutputHelper output) : base(output)
         {
         }
 
-        protected override IOutlierDetector CreateOutlierDetector(IReadOnlyList<double> values) => MadOutlierDetector.Create(values,
-            quantileEstimator: QuantileEstimator);
-
-        private static readonly IDictionary<string, TestData> TestDataMap = new Dictionary<string, TestData>
+        /// <summary>
+        /// Data cases for SimpleQuantileEstimator
+        /// </summary>
+        private static readonly IDictionary<string, TestData> SimpleQeTestDataMap = new Dictionary<string, TestData>
         {
             {"Yang_X0", new TestData(YangDataSet.X0, new double[] { })},
             {"Yang_X1", new TestData(YangDataSet.X1, new double[] {1000})},
             {"Yang_X2", new TestData(YangDataSet.X2, new double[] {500, 1000})},
             {"Yang_X3", new TestData(YangDataSet.X3, new double[] {1000})},
-            {"Yang_X4", new TestData(YangDataSet.X4, new double[] {300, 500, 1000, 1500})}
+            {"Yang_X4", new TestData(YangDataSet.X4, new double[] {300, 500, 1000, 1500})},
+            {"Real0", new TestData(RealDataSet.X0, new double[] {38594, 39075})},
+            {"Real1", new TestData(RealDataSet.X1, new double[] {0, 0, 0, 0, 1821})},
+            {"Real2", new TestData(RealDataSet.X2, new double[] {95, 4364})},
+            {"Real3", new TestData(RealDataSet.X3, new double[] {1067, 1085, 1133, 1643, 4642})},
+            {"Real4", new TestData(RealDataSet.X4, new double[] {14893, 15056, 15364, 15483, 16504, 17208, 17446})},
+            {"Beta0", new TestData(BetaDataSet.X0, new double[] {2308, 2504, 2556, 2569, 2591, 2604, 2754, 2899, 3262, 3325, 3329})},
+            {"Beta1", new TestData(BetaDataSet.X1, new double[] {3071})},
+            {"Beta2", new TestData(BetaDataSet.X2, new double[] {3642})},
+            {"MBeta_Lower1", new TestData(ModifiedBetaDataSet.Lower1, new double[] {-2000, 2919, 3612})},
+            {"MBeta_Lower2", new TestData(ModifiedBetaDataSet.Lower2, new double[] {-2001, -2000, 2919, 3612})},
+            {"MBeta_Lower3", new TestData(ModifiedBetaDataSet.Lower3, new double[] {-2002, -2001, -2000, 2919, 3612})},
+            {"MBeta_Upper1", new TestData(ModifiedBetaDataSet.Upper1, new double[] {2919, 3612, 6000})},
+            {"MBeta_Upper2", new TestData(ModifiedBetaDataSet.Upper2, new double[] {2919, 3612, 6000, 6001})},
+            {"MBeta_Upper3", new TestData(ModifiedBetaDataSet.Upper3, new double[] {2919, 3612, 6000, 6001, 6002})},
+            {"MBeta_Both0", new TestData(ModifiedBetaDataSet.Both0, new double[] {-2000, 2919, 3612, 6000})},
+            {"MBeta_Both1", new TestData(ModifiedBetaDataSet.Both1, new double[] {-2001, -2000, 2919, 3612, 6000, 6001})},
+            {"MBeta_Both2", new TestData(ModifiedBetaDataSet.Both2, new double[] {-2002, -2001, -2000, 2919, 3612, 6000, 6001, 6002})}
         };
 
-        [UsedImplicitly] public static TheoryData<string> TestDataKeys = TheoryDataHelper.Create(TestDataMap.Keys);
+        [UsedImplicitly] public static TheoryData<string> SimpleQeTestDataKeys = TheoryDataHelper.Create(SimpleQeTestDataMap.Keys);
 
         [Theory]
-        [MemberData(nameof(TestDataKeys))]
-        public void MadOutlierDetectorTest([NotNull] string testDataKey) => Check(TestDataMap[testDataKey]);
-
-        protected override void DumpDetails(TestData testData)
+        [MemberData(nameof(SimpleQeTestDataKeys))]
+        public void MadOutlierDetectorSimpleQeTest([NotNull] string testDataKey) => Check(SimpleQeTestDataMap[testDataKey], 
+            values => MadOutlierDetector.Create(values, quantileEstimator: SimpleQuantileEstimator.Instance));
+        
+        /// <summary>
+        /// Data cases for HarrellDavisQuantileEstimator
+        /// </summary>
+        private static readonly IDictionary<string, TestData> HdQeTestDataMap = new Dictionary<string, TestData>
         {
-            var values = testData.Values.ToSorted();
-            double median = QuantileEstimator.GetMedian(values);
-            double mad = MedianAbsoluteDeviation.CalcMad(values, quantileEstimator: QuantileEstimator);
+            {"Real0", new TestData(RealDataSet.X0, new double[] {38594, 39075})},
+            {"Real1", new TestData(RealDataSet.X1, new double[] {0, 0, 0, 0, 1821})},
+            {"Real2", new TestData(RealDataSet.X2, new double[] {95, 4364})},
+            {"Real3", new TestData(RealDataSet.X3, new double[] {1067, 1085, 1133, 1643, 4642})},
+            {"Real4", new TestData(RealDataSet.X4, new double[] {14893, 15056, 15364, 15483, 16504, 17208, 17446})},
+            {"Beta0", new TestData(BetaDataSet.X0, new double[] {2308, 2504, 2556, 2569, 2591, 2604, 2754, 2899, 3262, 3325, 3329})},
+            {"Beta1", new TestData(BetaDataSet.X1, new double[] {3071})},
+            {"Beta2", new TestData(BetaDataSet.X2, new double[] {3642})},
+            {"MBeta_Lower1", new TestData(ModifiedBetaDataSet.Lower1, new double[] {-2000, 2919, 3612})},
+            {"MBeta_Lower2", new TestData(ModifiedBetaDataSet.Lower2, new double[] {-2001, -2000, 2919, 3612})},
+            {"MBeta_Lower3", new TestData(ModifiedBetaDataSet.Lower3, new double[] {-2002, -2001, -2000, 2919, 3612})},
+            {"MBeta_Upper1", new TestData(ModifiedBetaDataSet.Upper1, new double[] {2919, 3612, 6000})},
+            {"MBeta_Upper2", new TestData(ModifiedBetaDataSet.Upper2, new double[] {2919, 3612, 6000, 6001})},
+            {"MBeta_Upper3", new TestData(ModifiedBetaDataSet.Upper3, new double[] {2919, 3612, 6000, 6001, 6002})},
+            {"MBeta_Both0", new TestData(ModifiedBetaDataSet.Both0, new double[] {-2000, 2919, 3612, 6000})},
+            {"MBeta_Both1", new TestData(ModifiedBetaDataSet.Both1, new double[] {-2001, -2000, 2919, 3612, 6000, 6001})},
+            {"MBeta_Both2", new TestData(ModifiedBetaDataSet.Both2, new double[] {-2002, -2001, -2000, 2919, 3612, 6000, 6001, 6002})}
+        };
 
-            Output.WriteLine($"Median = {median.ToString(TestCultureInfo.Instance)}");
-            Output.WriteLine($"MAD    = {mad.ToString(TestCultureInfo.Instance)}");
-        }
+        [UsedImplicitly] public static TheoryData<string> HdQeTestDataKeys = TheoryDataHelper.Create(HdQeTestDataMap.Keys);
+
+        [Theory]
+        [MemberData(nameof(HdQeTestDataKeys))]
+        public void MadOutlierDetectorHdQeTest([NotNull] string testDataKey) => Check(HdQeTestDataMap[testDataKey],
+            values => MadOutlierDetector.Create(values, quantileEstimator: HarrellDavisQuantileEstimator.Instance));
     }
 }
