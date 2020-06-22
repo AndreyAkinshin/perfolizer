f7133c1281800589b691d7b4110707e312d237c9
Andrey Akinshin <andrey.akinshin@jetbrains.com>
2020-06-22 20:08:59 +0300

    QuartilesTests

---
+++ src/Perfolizer/Perfolizer.Tests/Mathematics/QuantileEstimators/QuartilesTests.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using Perfolizer.Mathematics.QuantileEstimators;
+using Xunit;
+
+namespace Perfolizer.Tests.Mathematics.QuantileEstimators
+{
+    public class QuartilesTests
+    {
+        private void Check(
+            [JetBrains.Annotations.NotNull] IReadOnlyList<double> values,
+            [JetBrains.Annotations.NotNull] IReadOnlyList<double> expectedQuartiles,
+            [CanBeNull] IQuantileEstimator quantileEstimator)
+        {
+            var quartiles = Quartiles.Create(values, quantileEstimator);
+            Assert.Equal(expectedQuartiles[0], quartiles.Q0);
+            Assert.Equal(expectedQuartiles[1], quartiles.Q1);
+            Assert.Equal(expectedQuartiles[2], quartiles.Q2);
+            Assert.Equal(expectedQuartiles[3], quartiles.Q3);
+            Assert.Equal(expectedQuartiles[4], quartiles.Q4);
+
+            Assert.Equal(expectedQuartiles[0], quartiles.Min);
+            Assert.Equal(expectedQuartiles[2], quartiles.Median);
+            Assert.Equal(expectedQuartiles[4], quartiles.Max);
+            Assert.Equal(expectedQuartiles[3] - expectedQuartiles[1], quartiles.InterquartileRange);
+        }
+
+        [Fact]
+        [SuppressMessage("ReSharper", "AssignNullToNotNullAttribute")]
+        public void QuartileNullTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => Quartiles.Create(null));
+        }
+
+        [Fact]
+        public void QuartileTest00()
+        {
+            Check(new double[] {0, 0, 0, 0, 0}, new double[] {0, 0, 0, 0, 0}, SimpleQuantileEstimator.Instance);
+        }
+        
+        [Fact]
+        public void QuartileTest01()
+        {
+            Check(new double[] {0, 1, 2, 3, 4}, new double[] {0, 1, 2, 3, 4}, SimpleQuantileEstimator.Instance);
+        }
+        
+        [Fact]
+        public void QuartileTest02()
+        {
+            Check(new double[] {0, 1, 2, 3, 4}, new double[] {0, 1, 2, 3, 4}, SimpleQuantileEstimator.Instance);
+        }
+    }
+}
