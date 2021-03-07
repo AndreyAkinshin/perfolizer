a89c77f54a136bb84eea63cd69a90d4cecf77efd
Andrey Akinshin <andrey.akinshin@gmail.com>
2021-03-07 09:32:43 +0300

    Add GeneralizedLambdaDistribution

---
+++ src/Perfolizer/Perfolizer/Mathematics/Distributions/ContinuousDistributions/GeneralizedLambdaDistribution.cs
@@ -0,0 +1,42 @@
+using System;
+using Perfolizer.Mathematics.Common;
+using Perfolizer.Mathematics.Randomization;
+using static System.Math;
+
+namespace Perfolizer.Mathematics.Distributions.ContinuousDistributions
+{
+    /// <summary>
+    /// Generalized lambda distribution
+    /// <remarks>
+    /// The distribution is defined by its quantile function:
+    /// Q(p) = mu + sigma * (p^a - (1 - p)^b)
+    /// </remarks>
+    /// </summary>
+    public class GeneralizedLambdaDistribution : IContinuousDistribution
+    {
+        public double Mu { get; }
+        public double Sigma { get; }
+        public double A { get; }
+        public double B { get; }
+
+        public GeneralizedLambdaDistribution(double mu, double sigma, double a, double b)
+        {
+            Mu = mu;
+            Sigma = sigma;
+            A = a;
+            B = b;
+        }
+
+        public double Pdf(double x) => throw new NotImplementedException();
+        public double Cdf(double x) => throw new NotImplementedException();
+
+        public double Quantile(Probability p) => Mu + Sigma * (Pow(p, A) - Pow(1 - p, B));
+
+        public RandomGenerator Random(Random random = null) => new DistributionRandomGenerator(this, random);
+
+        public double Mean => throw new NotImplementedException();
+        public double Median => throw new NotImplementedException();
+        public double Variance => throw new NotImplementedException();
+        public double StandardDeviation => throw new NotImplementedException();
+    }
+}
