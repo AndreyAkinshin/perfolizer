6f6ac89d388ece90ad7f25fd32d43bf7a86db74f
Andrey Akinshin <andrey.akinshin@gmail.com>
2021-12-16 18:56:29 +0300

    Add GreenwaldKhannaSpecificQuantileEstimator

---
+++ src/Perfolizer/Perfolizer/Mathematics/QuantileEstimators/GreenwaldKhannaSpecificQuantileEstimator.cs
@@ -0,0 +1,26 @@
+using Perfolizer.Mathematics.Common;
+
+namespace Perfolizer.Mathematics.QuantileEstimators
+{
+    public class GreenwaldKhannaSpecificQuantileEstimator : ISequentialSpecificQuantileEstimator
+    {
+        private readonly GreenwaldKhannaQuantileEstimator estimator;
+        private readonly Probability probability;
+
+        public GreenwaldKhannaSpecificQuantileEstimator(Probability probability, double epsilon)
+        {
+            estimator = new GreenwaldKhannaQuantileEstimator(epsilon);
+            this.probability = probability;
+        }
+
+        public void Add(double value)
+        {
+            estimator.Add(value);
+        }
+
+        public double GetQuantile()
+        {
+            return estimator.GetQuantile(probability);
+        }
+    }
+}
