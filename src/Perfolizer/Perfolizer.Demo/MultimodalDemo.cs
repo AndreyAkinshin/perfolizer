31d80c13b399e31aa69db6a8e164f0f25d70bbaf
Andrey Akinshin <andrey.akinshin@gmail.com>
2021-12-16 18:38:39 +0300

    Update MultimodalDemo

--- src/Perfolizer/Perfolizer.Demo/MultimodalDemo.cs
+++ src/Perfolizer/Perfolizer.Demo/MultimodalDemo.cs
@@ -1,31 +1,54 @@
 using System;
 using System.Collections.Generic;
+using Perfolizer.Common;
 using Perfolizer.Mathematics.Distributions.ContinuousDistributions;
 using Perfolizer.Mathematics.Multimodality;
 
 namespace Perfolizer.Demo
 {
-    public class MultimodalDemo: IDemo
+    public class MultimodalDemo : IDemo
     {
         public void Run()
         {
+            // In order to detect multimodal distributions, we can use the Lowland multimodality detector
+            // * Akinshin, Andrey. "Lowland multimodality detection."
+            //   https://aakinshin.net/posts/lowland-multimodality-detection/
+            var modalityDetector = LowlandModalityDetector.Instance;
+            // We also can use a plain-text notation to describe the details
+            // * Akinshin, Andrey. "Plain-text summary notation for multimodal distributions."
+            //   https://aakinshin.net/posts/modality-summary-notation/
+            var modalityDataFormatter = ManualModalityDataFormatter.Default;
+
+            void DetectModes(string title, Sample sample)
+            {
+                var modalityData = modalityDetector.DetectModes(sample);
+                Console.WriteLine("Distribution : " + title);
+                Console.WriteLine("Modality     : " + modalityData.Modality);
+                Console.WriteLine("Details      : " + modalityDataFormatter.Format(modalityData));
+                Console.WriteLine();
+            }
+
             var random = new Random(42);
-            var data = new List<double>();
-            data.AddRange(new NormalDistribution(mean: 20, stdDev: 1).Random(random).Next(200));
-            data.AddRange(new NormalDistribution(mean: 22, stdDev: 1).Random(random).Next(200));
-            List<double>
-                unimodal = new List<double>(),
-                bimodal = new List<double>(),
-                trimodal = new List<double>();
-            unimodal.AddRange(new NormalDistribution(mean: 20, stdDev: 1).Random(random).Next(200));
-            bimodal.AddRange(new NormalDistribution(mean: 20, stdDev: 1).Random(random).Next(200));
-            bimodal.AddRange(new NormalDistribution(mean: 30, stdDev: 1).Random(random).Next(200));
-            trimodal.AddRange(new NormalDistribution(mean: 20, stdDev: 1).Random(random).Next(200));
-            trimodal.AddRange(new NormalDistribution(mean: 30, stdDev: 1).Random(random).Next(200));
-            trimodal.AddRange(new NormalDistribution(mean: 40, stdDev: 1).Random(random).Next(200));
-            Console.WriteLine("Unimodal : " + MValueCalculator.Calculate(unimodal));
-            Console.WriteLine("Bimodal  : " + MValueCalculator.Calculate(bimodal));
-            Console.WriteLine("Trimodal : " + MValueCalculator.Calculate(trimodal));
+
+            Sample Generate(int n, params IContinuousDistribution[] distributions)
+            {
+                var values = new List<double>();
+                foreach (var distribution in distributions)
+                    values.AddRange(distribution.Random(random).Next(n));
+                return new Sample(values);
+            }
+
+            DetectModes("Standard normal distribution", Generate(200,
+                NormalDistribution.Standard));
+            DetectModes("Standard uniform distribution", Generate(200,
+                UniformDistribution.Standard));
+            DetectModes("Bimodal distribution", Generate(200,
+                new NormalDistribution(10),
+                new NormalDistribution(20)));
+            DetectModes("Trimodal distribution", Generate(200,
+                new NormalDistribution(10),
+                new NormalDistribution(20),
+                new NormalDistribution(30)));
         }
     }
 }
