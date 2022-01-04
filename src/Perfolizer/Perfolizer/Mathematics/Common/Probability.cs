9acef7c45af03156369a6b4f9b3132049760eb30
Andrey Akinshin <andrey.akinshin@gmail.com>
2022-01-04 15:55:56 +0300

    Update Probability

--- src/Perfolizer/Perfolizer/Mathematics/Common/Probability.cs
+++ src/Perfolizer/Perfolizer/Mathematics/Common/Probability.cs
@@ -4,7 +4,7 @@ using Perfolizer.Common;
 
 namespace Perfolizer.Mathematics.Common
 {
-    public readonly struct Probability
+    public readonly struct Probability : IComparable, IComparable<Probability>, IEquatable<Probability>, IFormattable
     {
         public static readonly Probability Zero = 0.0;
         public static readonly Probability Half = 0.5;
@@ -22,21 +22,49 @@ namespace Perfolizer.Mathematics.Common
         public static implicit operator double(Probability probability) => probability.Value;
         public static implicit operator Probability(double value) => new Probability(value);
 
+        [NotNull]
         public override string ToString()
         {
             return Value.ToString(DefaultCultureInfo.Instance);
         }
-        
+
+        public int CompareTo(object obj)
+        {
+            return Value.CompareTo(obj);
+        }
+
+        [NotNull]
         public string ToString(IFormatProvider formatProvider)
         {
             return Value.ToString(formatProvider);
         }
 
-        public string ToString(string format, IFormatProvider formatProvider = null)
+        public string ToString(string format, [CanBeNull] IFormatProvider formatProvider = null)
         {
             return Value.ToString(format, formatProvider ?? DefaultCultureInfo.Instance);
         }
 
+        public bool Equals(Probability other)
+        {
+            return Value.Equals(other.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Probability other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public int CompareTo(Probability other)
+        {
+            return Value.CompareTo(other.Value);
+        }
+
+        [CanBeNull]
         public static Probability[] ToProbabilities([CanBeNull] double[] values)
         {
             if (values == null)
