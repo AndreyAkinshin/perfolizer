5a1baa1a9f43d223f08b467e13e23d8da130ea41
Andrey Akinshin <andrey.akinshin@gmail.com>
2025-10-29 11:55:29 +0100

    Make BinomialCoefficientHelper thread-safe

--- src/Perfolizer/Perfolizer/Mathematics/Common/BinomialCoefficientHelper.cs
+++ src/Perfolizer/Perfolizer/Mathematics/Common/BinomialCoefficientHelper.cs
@@ -7,31 +7,32 @@ public static class BinomialCoefficientHelper
 {
     public const int MaxAcceptableN = 65;
 
-    private static long[,]? pascalTriangle;
+    private static readonly Lazy<long[,]> PascalTriangle = new(BuildPascalTriangle);
+
+    private static long[,] BuildPascalTriangle()
+    {
+        checked
+        {
+            long[,] triangle = new long[MaxAcceptableN + 1, MaxAcceptableN + 1];
+            for (int i = 0; i <= MaxAcceptableN; i++)
+            {
+                triangle[i, 0] = 1;
+                for (int j = 1; j <= i; j++)
+                    triangle[i, j] = triangle[i - 1, j - 1] + triangle[i - 1, j];
+            }
+
+            return triangle;
+        }
+    }
 
     public static long BinomialCoefficient(int n, int k)
     {
-        const int maxN = MaxAcceptableN;
-        if (n < 0 || n > maxN)
+        if (n < 0 || n > MaxAcceptableN)
             throw new ArgumentOutOfRangeException(nameof(n));
         if (k < 0 || k > n)
             return 0;
 
-        if (pascalTriangle == null)
-        {
-            checked
-            {
-                pascalTriangle = new long[maxN + 1, maxN + 1];
-                for (int i = 0; i <= maxN; i++)
-                {
-                    pascalTriangle[i, 0] = 1;
-                    for (int j = 1; j <= i; j++)
-                        pascalTriangle[i, j] = pascalTriangle[i - 1, j - 1] + pascalTriangle[i - 1, j];
-                }
-            }
-        }
-
-        return pascalTriangle[n, k];
+        return PascalTriangle.Value[n, k];
     }
 
     /// <summary>
@@ -41,10 +42,10 @@ public static class BinomialCoefficientHelper
     {
         Assertion.Positive(nameof(n), n);
         Assertion.InRangeInclusive(nameof(k), k, 0, n);
-            
+
         return FactorialFunction.LogValue(n) - FactorialFunction.LogValue(k) - FactorialFunction.LogValue(n - k);
     }
-    
+
     public static double BinomialCoefficient(double n, double k)
     {
         Assertion.Positive(nameof(n), n);
